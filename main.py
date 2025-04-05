fcaba176048c4c92e3fcd9e61e5d8360a2484b64
Nehrovin <hobbiesmj@icloud.com>
2025-04-05 13:58:36 +0200

    CH3 L3 done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import num_of_words
 from stats import count_characters
 from stats import sort_list
+import sys 
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +9,11 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    filepath = "./books/frankenstein.txt"
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
     word_count = num_of_words(get_book_text(filepath))
     num_of_characters = count_characters(get_book_text(filepath))
     list_to_report = sort_list(num_of_characters)
